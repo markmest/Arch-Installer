35ef98389b48f4e4a83b38d3e083bd03aeb1ad9f
markmest <marko.mestrovic3@gmail.com>
2023-12-26 00:45:50 +0100

    First part of the base script

--- base.sh
+++ base.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Is this working?"
+echo "-------------------------------------------------"
+echo "--Setting up mirrors for optimal download speed--"
+echo "-------------------------------------------------"
+timedatectl set-ntp true
+loadkeys croat
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+reflector --country Croatia --age 6 --sort rate --save /etc/pacman.d/mirrorlist
+pacman-key --init 
+pacman-key --populate
+pacman -Syyy
+echo " "
+lsblk
+
 
