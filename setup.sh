152aa19391e7b6e34aa0a5349234fc25e65cc1cc
markmest <marko.mestrovic3@gmail.com>
2023-12-26 14:25:50 +0100

    Messing with pacman

--- setup.sh
+++ setup.sh
@@ -8,12 +8,13 @@ echo "Setting timezone and hardware clock...."
 ln -sf /usr/share/zoneinfo/Europe/Zagreb /etc/localtime
 hwclock --systohc
 
-echo "Generating locale..."
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/#en_US ISO-8859-1/en_US ISO-8859-1/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "Setting default keymap in an interactive session..."
 echo "KEYMAP=croat" >> /etc/vconsole.conf
+echo " "
 
 echo "-------------------------------------------------"
 echo "----------Setting hostname and hosts-------------"
@@ -24,12 +25,25 @@ read -p "Choose a (creative) hostname: " HOSTNAME
 echo $HOSTNAME >> /etc/hostname
 
 echo "Setting hosts file."
-cat > /mnt/etc/hosts <<EOF
+cat > /etc/hosts <<EOF
 127.0.0.1   localhost
 ::1         localhost
 127.0.1.1   $HOSTNAME.localdomain   $HOSTNAME
 EOF
 
-read -p "Root password: " ROOT_PASS
+read -p "Root password: "
 echo "root:$ROOT_PASS" | chpasswd
+
+PGKS=(
+	'neofetch'
+	'neovim'
+	)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+
+
 
